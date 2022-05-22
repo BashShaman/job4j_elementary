e041c30b7b738ec55c181bfee5f91ed2c22e318c
BashW <BashWhiteShaman@yandex.com>
2022-05-22 15:33:26 +0700

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Savelyev Vladislav");
+        System.out.println("1.04.1997");
+    }
+}

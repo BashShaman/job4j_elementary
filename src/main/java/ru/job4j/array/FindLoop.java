e688a326a3fe5a631c8dcd65ad93a5ad5fd806e8
BashW <BashWhiteShaman@yandex.com>
2022-05-27 15:40:22 +0700

    6.5. Поиск индекса в диапазоне. [#156318]

--- src/main/java/ru/job4j/array/FindLoop.java
+++ src/main/java/ru/job4j/array/FindLoop.java
@@ -11,4 +11,17 @@ public class FindLoop {
         }
         return rst;
     }
+
+    public static int indexInRange(int[] data, int el, int start, int finish) {
+        int rst = -1;
+        int indexStart = (start > -1) ? start : 0;
+        int indexFinish = (finish < data.length) ? finish : data.length - 1;
+        for (int index = indexStart; index <= indexFinish; index++) {
+            if (data[index] == el) {
+                rst = index;
+                break;
+            }
+        }
+        return rst;
+    }
 }

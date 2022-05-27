3279d6a0d99c3df31e1260509878d2b21a6327ab
BashW <BashWhiteShaman@yandex.com>
2022-05-27 13:11:18 +0700

    5.7. Ипотека [#156315]

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -4,13 +4,11 @@ public class Mortgage {
     public static int year(double amount, int salary, double percent) {
         int year = 0;
         double factor = 1 + percent / 100;
-
         while (amount > 0) {
             amount *= factor;
             amount -= salary;
             year += 1;
         }
-
         return year;
     }
 }
